d77567b62bcc57f52e9f4851d78ae2517b29751f
GEETHA-VENKAT <88129675+GEETHA-VENKAT@users.noreply.github.com>
2024-01-03 18:30:27 +0530

    Create Integer.py

---
+++ Integer.py
@@ -0,0 +1,31 @@
+num1 = 19
+num2 = 15
+
+# Basic Arithmetic
+result1 = num1 + num2
+print(result1)
+# Substraction
+result2 = num1 - num2 
+print(result2)
+# Multiplication.
+result3 = num1 * num2
+print(result3)
+# Division
+result4 = num1 / num2
+print(result4)
+
+# Floating Data Types.
+
+num1 = 12.13
+num2 = 9.12
+
+# Addition.
+result = num1 + num2
+print(result)
+# Substraction
+result2 = num1 - num2
+print(result2)
+result3 = num1 * num2
+print("Multiplicaiton of num1 and num2:", result3)
+result4 = num1 / num2
+print("The division of the num1 and num2", result4)
